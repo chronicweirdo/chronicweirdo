667082782ca4a64afff46c1abddfed41030f20e4
chronicweirdo <stretchypowers@pm.me>
2026-05-01 21:08:58 +0300

    scripts for hiding headers

---
+++ tarot/script.js
@@ -0,0 +1,79 @@
+function applyParentBasedHideToggles(className) {
+    elements = document.getElementsByClassName(className)
+    for (element of elements) {
+        applyHideToggle(element)
+    }
+}
+
+function applyParentBasedHideToggle(element) {
+    let parent = element.parentElement
+    let anchorElement = element
+    let invisibleDisplay = "none"
+    let originalDisplays = new Map()
+
+    let toggle = () => {
+        for (child of parent.children) {
+            if (child != anchorElement) {
+                if (child.style.display != invisibleDisplay) {
+                    originalDisplays.set(child, child.style.display)
+                    child.style.display = invisibleDisplay
+                } else {
+                    child.style.display = originalDisplays.get(child)
+                }
+            }
+        }
+    }
+    anchorElement.onclick = (event) => {toggle()}
+}
+
+function getHeaderLevel(element) {
+    if (element != null && element.tagName.startsWith("H")) {
+        let level = parseInt(element.tagName.substr(1))
+        if (level != NaN) {
+            return level
+        }
+    }
+    return null
+}
+
+function applyHeaderLevelHideToggle(element) {
+    let anchorLevel = getHeaderLevel(element)
+    if (anchorLevel == null) return;
+
+    //console.log("applying toggle")
+    //console.log(element)
+    let anchorElement = element
+
+    let invisibleDisplay = "none"
+    let originalDisplays = new Map()
+
+    let hide = true
+
+    let toggle = () => {
+        sibling = anchorElement.nextElementSibling
+        siblingLevel = getHeaderLevel(sibling)
+        while (sibling != null && (siblingLevel == null || siblingLevel > anchorLevel)) {
+            if (hide) {
+                originalDisplays.set(sibling, sibling.style.display)
+                sibling.style.display = invisibleDisplay
+            } else {
+                sibling.style.display = originalDisplays.get(sibling)
+            }
+            sibling = sibling.nextElementSibling
+            siblingLevel = getHeaderLevel(sibling)
+        }
+        hide = ! hide
+    }
+
+    anchorElement.onclick = (event) => {toggle()}
+    anchorElement.style.cursor = "pointer"
+    toggle()
+}
+
+function setupHeaderToggles() {
+    for (tagName of ["H4", "h3"]) {
+        for (el of document.getElementsByTagName(tagName)) {
+            applyHeaderLevelHideToggle(el)
+        }
+    }
+}
